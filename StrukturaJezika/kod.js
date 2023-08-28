ed91594b5e90b251bfa65e93e607cef1cea7838f
Dinko Sulicek <dinko.sulicek@predavaci.algebra.hr>
2023-08-28 21:11:16 +0200

    kraj JS

---
+++ StrukturaJezika/kod.js
@@ -0,0 +1,51 @@
+var a = 5;
+var firstName = "Pero";
+lastName = "Majdić";
+
+if (x) {
+  console.log(x);
+}
+
+if (x) {
+  console.log(x);
+}
+
+cijena = 19.9;
+discount = 0.3;
+
+price = (cijena * 55) / discount;
+
+var a = 5;
+var firstName = "Pero";
+lastName = "Majdić";
+
+if (x) {
+  console.log(x);
+}
+
+if (x) {
+  console.log(x);
+}
+cijena = 19.9;
+discount = 0.3;
+
+price = (cijena * 55) / discount;
+
+/**
+// Inicijalizacija varijabli?
+*/
+var first_name = "Tony";
+
+var a, b, c;
+var first_name = "John",
+  lastName = Doe,
+  price = 0,
+  discount = 78,
+  fullPrice = 0,
+  $myObject = {};
+console.log(price);
+console.log(lastName);
+
+/*
+Kraj mog
+programa */
