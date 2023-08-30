9295d6c6e637120c2de69702a26461e9848d1ecc
Dinko Sulicek <dinko.sulicek@predavaci.algebra.hr>
2023-08-30 20:17:48 +0200

    rješenje

--- RadSTekstom_30_8_2023/zadatakTekst.js
+++ RadSTekstom_30_8_2023/zadatakTekst.js
@@ -9,3 +9,21 @@
 
 var text = ' Lorem ipsum dolor sit amet';
 
+//   1. Spremite duljinu stringa u varijablu.
+var length = text.length;
+console.log(length)
+//   2. Izdvojite riječ 'sit' u zasebnu varijablu.
+var wordSit = text.substring(19, 22); // Ili split, splice
+console.log(wordSit)
+//   3. Zamijenite riječ 'amet' sa riječi 'elit'.
+console.log(text.replace('amet', 'elit')); 
+//   4. Konkatenirajte u novu varijablu zadani string sa stringom 'consectetur adipiscing elit', sa zarezom izmedu.
+var newText = text + ', consectetur adipiscing elit';
+console.log(newText)
+//   5. Konvertirajte sve riječi u orginalnom stringu u velika slova
+console.log(text.toUpperCase());
+//   6. Maknite počenu prazninu u stringu
+console.log(text.trim());
+//   7. Nađite slovo na poziciji 12
+console.log(text.charAt(12));
+
