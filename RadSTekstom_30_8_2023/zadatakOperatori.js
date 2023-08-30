2d72f93a63af156b40d5da85cae29ec227ab6488
Dinko Sulicek <dinko.sulicek@predavaci.algebra.hr>
2023-08-30 21:06:12 +0200

    rješenjeZad2

--- RadSTekstom_30_8_2023/zadatakOperatori.js
+++ RadSTekstom_30_8_2023/zadatakOperatori.js
@@ -21,9 +21,15 @@ const taxRate = 0.082;
 let totalPrice = price + (price * taxRate);
 totalPrice.toFixed(2);
 
-console.log("Total:", totalPrice);
+console.log("Total:", totalPrice.toFixed(2));
 
 
 // 1. Koja je vrijednost varijable trueFalse? Čemu služe dvostruki negacijski znakovi?
 
 // 2., 3. i 4. Koju će vrijednost ispisati console.log? Zašto?
+
+
+// 1. Usporedba vraća true. Negacija na stringu(truthy vrijednost) vraća true, bez obzira na sadržaj stringa, a negacija true vrijednosti je false. Obje strane su jednake.
+// 2. Ispisati će x vrijednost (Lorem ipsum31), jer je to prva truthy vrijednost.
+// 3. 5, jer num-- === 6 - 1
+// 4. 28.67
