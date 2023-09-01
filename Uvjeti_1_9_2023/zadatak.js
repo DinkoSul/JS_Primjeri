33b34990a0ee48e4542c4f39673cfc730aaa6556
Dinko Sulicek <dinko.sulicek@predavaci.algebra.hr>
2023-09-01 21:10:01 +0200

    DodatnoDZ

--- Uvjeti_1_9_2023/zadatak.js
+++ Uvjeti_1_9_2023/zadatak.js
@@ -4,3 +4,6 @@
 // var x = z === 2? y : 5;
 
 // 3. Napišite petlju koja će prolaziti kroz brojeve od 1 do 20. Za svaku će iteraciju provjeriti je li trenutni broj paran ili neparan, i console.log-ati rezultat (npr. "Broj 2 je paran").
+
+// 4. Napišite program koji će ispisati sve brojeve između 0 i 30 djeljive sa 7
+// 5. Napišite program koji će zbrojiti sve brojeve između 0 i 100 dijeljive sa 3
