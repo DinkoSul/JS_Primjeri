6611ae4305fc26254386f22a90fa6c26f07d64fb
Dinko Sulicek <dinko.sulicek@predavaci.algebra.hr>
2023-09-01 20:14:55 +0200

    zadatak1-dodatak

--- Uvjeti_1_9_2023/petlja_for.js
+++ Uvjeti_1_9_2023/petlja_for.js
@@ -1,7 +1,7 @@
 // for (var i = 0; i < 10; i++) {
 //   console.log(index);
 // }
-
+// 2. Koja je vrijednost isprintana u konzoli?
 var varOne = 0;
 for (i = 0; i < 4; i++) {
   for (j = 0; j < 4; j++) {
