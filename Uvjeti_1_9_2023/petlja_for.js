2801189401928215893d669970a763a129042ac1
Dinko Sulicek <dinko.sulicek@predavaci.algebra.hr>
2023-09-01 20:14:18 +0200

    zadatak1

---
+++ Uvjeti_1_9_2023/petlja_for.js
@@ -0,0 +1,11 @@
+// for (var i = 0; i < 10; i++) {
+//   console.log(index);
+// }
+
+var varOne = 0;
+for (i = 0; i < 4; i++) {
+  for (j = 0; j < 4; j++) {
+    varOne++;
+  }
+}
+console.log(varOne);
