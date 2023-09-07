f0658434a111f5d0b9fa94d93ab26c57a1795b03
Dinko S <dinko.sulicek@predavaci.algebra.hr>
2023-09-07 20:05:28 +0200

    objekti

---
+++ Objekti_7_9_2023/objekt1.js
@@ -0,0 +1,30 @@
+var cat = {};
+var dog = {
+  breed: "Golden retriever",
+  bark: function () {
+    return "Wuf Wuf";
+  },
+  "has Toy": false,
+};
+console.log(cat);
+console.log(dog.breed);
+console.log(dog.bark);
+console.log(dog.bark());
+
+dog.breed = "Pekinezer";
+console.log(dog.breed);
+
+//DZ za pročitati: https://medium.com/@shrijan00003/javascript-object-properties-writable-enumerable-configurable-eced27378256
+
+var igracka = dog["has Toy"];
+console.log(igracka);
+
+cat.color = "grey";
+cat.size = 30;
+console.log(cat);
+
+delete dog.bark;
+console.log(dog.propertyIsEnumerable(["has toy"]));
+
+var jsonda = JSON.stringify(dog);
+console.log(jsonda);
