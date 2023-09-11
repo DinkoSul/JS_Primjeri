50a099fabeb4f0a5450c614af3f7067b98dac77b
Dinko S <dinko.sulicek@predavaci.algebra.hr>
2023-09-11 20:49:15 +0200

    DZ1

---
+++ Objekti_11_9_2023/rijeke.js
@@ -0,0 +1,14 @@
+var footer = document.getElementById("footer");
+console.log(footer);
+var description = document.querySelector(".description");
+console.log(description);
+
+var d = document.createElement("div"); //kreirati novi element
+footer.appendChild(d); //dodijeliti novi element postojećem
+description.parentNode.removeChild(description); //brisanje elementa. parentNode - postavljam se na parent element elemnta koji želim obrisati
+
+//DZ:
+// HTML rijeke.html-a treba promijeniti pomocu JS i HTML DOM API-ja tako da:
+// 	- se na početak doda h1 element
+// 	- ispod diva sa klasom "description" se umetne novi div sa klasom "info"
+// 	- lista u footer elementu na kraju mora sadrzavati 5 praznih <li> elemenata - koristiti petlju!
