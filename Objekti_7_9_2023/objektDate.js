aa0c8ab26b7c3f9fd97c267f368f5a6b9e9ca028
Dinko S <dinko.sulicek@predavaci.algebra.hr>
2023-09-11 18:46:39 +0200

    Math

---
+++ Objekti_7_9_2023/objektDate.js
@@ -0,0 +1,13 @@
+//više informacija: https://www.w3schools.com/jsref/jsref_obj_date.asp
+const d = new Date(200000000000);
+//from Jan 1, 1970.
+console.log(d);
+
+var e = new Date(2023, 11, 24, 10, 33, 30, 0);
+console.log(e);
+
+var f = new Date("September 13, 2023 11:13:00");
+console.log(f);
+
+var g = new Date();
+console.log(g);
--- Objekti_7_9_2023/zadatakObjekt.js
+++ Objekti_7_9_2023/zadatakObjekt.js
@@ -4,3 +4,72 @@
 // 2. Sa petljom pristupi svim brojivim svojstvima tvog auto objekta i ispiši vrijednosti u konzoli zajedno sa njihovim imenima (kljucevima) (npr. ime: Ivan)
 // 3. Ispiši u konzoli razumljivu rečenicu koja uključuje bar 2 svojstva tvog objekta.
 // 4. Pretvori svoj objekt u JSON string.
+
+var auto = {
+  brend: {
+    tip: "Volvo",
+    model: "V40",
+  },
+  boja: "crvena",
+  brzina: 60,
+  maxBrzina: 160,
+  godinaProizvodnje: 1998,
+  kretanje: function () {
+    return "Auto se kreće brzinom: " + this.brzina;
+  },
+
+  ubrzaj: function (brojSekundi) {
+    while (brojSekundi && this.brzina <= this.maxBrzina) {
+      this.brzina += 5;
+      brojSekundi--;
+    }
+    return this.brzina;
+  },
+  zakoci: function (brojSekundi) {
+    while (brojSekundi && this.brzina > 0) {
+      this.brzina -= 20;
+      brojSekundi--;
+    }
+    return this.brzina;
+  },
+  promijeniBoju: function (boja) {
+    this.boja = boja;
+    return this.boja;
+  },
+};
+
+console.log(auto.kretanje());
+
+// console.log(auto.zakoci(4));
+// console.log(auto.ubrzaj(10));
+
+// for (const key in auto) {
+//   if (Object.hasOwnProperty.call(auto, key)) {
+//     const element = auto[key];
+//     console.log(key + ":" + element);
+//   }
+// }
+Object.defineProperty(auto, "kretanje", { enumerable: false });
+
+for (const key in auto) {
+  if (Object.hasOwnProperty.call(auto, key)) {
+    const element = auto[key];
+
+    if (auto.propertyIsEnumerable(key)) {
+      console.log(key + ": " + element);
+    }
+  }
+}
+
+// for (key in auto) {
+//   const value = auto[key];
+
+//   if (auto.propertyIsEnumerable(key)) {
+//     console.log(key + ": " + value);
+//   }
+// }
+
+console.log("Boja mog " + auto.brend.tip + " auta je " + auto.boja);
+// auto.promijeniBoju("žuta");
+console.log("Boja mog " + auto.brend.tip + " auta je " + auto.boja);
+console.log(JSON.stringify(auto));
