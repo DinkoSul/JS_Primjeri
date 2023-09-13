71be90af91988ea64416cea9c8f3b623942e34ed
Dinko S <dinko.sulicek@predavaci.algebra.hr>
2023-09-13 21:02:35 +0200

    dz

---
+++ DOM_13_9_2023/dz.js
@@ -0,0 +1,4 @@
+// Stil elemenata treba promijeniti pomocu JS i HTML DOM API-ja tako da:
+// 	- umjesto circle buleta na elementima liste prikažu redne brojke
+// 	- div sa klasom info treba dobiti klasu koja će ga sakriti (dodajte u stylesheet)
+// 	- saznajte margin-left izračunati stil koji je primijenjen na body elementu
