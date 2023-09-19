a2f88b853ae74029c22832703d450968f8291266
Dinko S <dinko.sulicek@predavaci.algebra.hr>
2023-09-19 20:47:16 +0200

    dz es6

---
+++ 19_9_2023/dz_es6.js
@@ -0,0 +1,7 @@
+// https://codeshare.io/MNlBKe
+
+// 1. Postavite “use strict” direktivu na početak programa u scripts.js. Ispravite sve greške koje se javljaju.
+
+// 2. Dodajte polje "user" u "users" pomoću spread operatora. Spremite rezultat u novu varijablu i ispisite rezultat.
+
+// 3.Koju vrijednost vraćaju console.log izjave? Probajte zamijeniti let sa var.
