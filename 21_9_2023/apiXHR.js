549ac8855a14104c07668f79ab5c04072dad3b08
Dinko S <dinko.sulicek@predavaci.algebra.hr>
2023-09-21 20:18:41 +0200

    ApiXhr poziv i ispis

--- 21_9_2023/apiXHR.js
+++ 21_9_2023/apiXHR.js
@@ -3,10 +3,11 @@
 // https://www.ics.uci.edu/~fielding/pubs/dissertation/rest_arch_style.htm
 // https://www.w3schools.com/xml/ajax_intro.asp
 var request = new XMLHttpRequest();
-request.open("GET", endpoint, true);
+request.open("GET", "https://api.punkapi.com/v2/beers", true);
 
 request.onload = function () {
   // Pristupi podacima koje je server vratio
+  console.log(request);
 };
 
 request.send();
