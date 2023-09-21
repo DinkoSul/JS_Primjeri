6e89cf10ec51e095bbf1517330d28a55c4de0935
Dinko S <dinko.sulicek@predavaci.algebra.hr>
2023-09-21 19:26:17 +0200

    promise varijante

--- 21_9_2023/promises_js.js
+++ 21_9_2023/promises_js.js
@@ -1,13 +1,34 @@
-function getX() {
-  return Math.random() >= 0.5;
-}
+// const getX = () => {
+//   let a = Math.random();
+//   console.log(a);
+//   a >= 0.5;
+// };
+//dio s ispisom
+// function getX() {
+//   let a = Math.random();
+//   console.log(a);
+//   return a >= 0.5;
+// }
+
+// var wait = new Promise((resolve, reject) => {
+//   const x = getX();
+//   if (x) {
+//     return setTimeout(resolve, 1000);
+//   }
+//   return setTimeout(reject, 1000);
+// })
+//   .then(() => console.log("Yay!"))
+//   .catch(() => console.log("Oh no!"));
+
+var getX = () => Math.random() >= 0.5;
 
 var wait = new Promise((resolve, reject) => {
   const x = getX();
+
   if (x) {
-    return setTimeout(resolve, 1000);
+    return setTimeout(() => resolve(x), 1000);
   }
   return setTimeout(reject, 1000);
 })
-  .then(() => console.log("Yay!"))
+  .then((value) => console.log(value))
   .catch(() => console.log("Oh no!"));
