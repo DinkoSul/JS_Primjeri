f15f1d828d103a2ac11ee66d56f66471814576fa
Dinko S <dinko.sulicek@predavaci.algebra.hr>
2023-10-05 20:34:20 +0200

    Ponavljanje JS

---
+++ 5_10_2023/ponavljanje.js
@@ -0,0 +1,28 @@
+// Zada 1
+// Napišite JavaScript funkciju koja prihvaća cjelobrojnu vrijednost kao argument te vraća poruku da li se radi o parnom ili neparnom broju.
+// Pretpostavite da će argument uvijek biti cjelobrojna vrijednost.
+
+// Zad2:
+// Napišite JavaScript funkciju koja prihvaća zadani niz kao argument i pronalazi najdužu riječ unutar niza. Vratite tu riječ iz funkcije i spremite u varijablu čiju ćete vrijednost ispisati u konzoli.
+
+var rijeci = [
+  "quickest",
+  "brown",
+  "fox",
+  "jumps",
+  "over",
+  "the",
+  "lazy",
+  "dog",
+];
+
+// Zad3: isto kao 2 samo najkraću riječ
+
+//Zad 4
+// Koristeći JavaScript dohvatite HTML element preko njegova ID-a "zaglavlje" te mu dodajte novi podelement tipa <h1> s tekstom "Naslov" (novi element biti će posljednji podelement svog roditelja).
+
+//ZAd 5:
+// Koristeći JavaScript dohvatite HTML element preko njegova ID-a "zaglavlje" te ga uklonite iz HTML stabla.
+
+//Zad6:
+//Koristeći JavaScript dohvatite HTML element preko njegova ID-a "zaglavlje" te mu postavite sljedeću pozadinsku boju: "#ddd".
